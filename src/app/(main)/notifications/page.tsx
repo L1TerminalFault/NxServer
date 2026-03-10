1498ee6ee198e99f1a952a689f6164cec5510c42
x <x>
2026-03-10 17:45:37 +0300

    making sure sublist won't be possibly null

--- src/app/(main)/notifications/page.tsx
+++ src/app/(main)/notifications/page.tsx
@@ -45,14 +45,14 @@ export default function Notification() {
   const refresh = async () => {
     setRefreshing(true);
     if (polling) return; // console.log("refresh cancelled because of lock poll");
-    await fetchBase(sublist[0]);
+    await fetchBase(sublist[0] || "");
     setRefreshing(false);
   };
 
   const poll = async () => {
     if (refreshing || polling) return; // console.log("skipping poll because lock is held");
     setPolling(true);
-    await fetchBase(sublist[0]);
+    await fetchBase(sublist[0] || "");
     setPolling(false);
     setRefreshing(false);
   };
